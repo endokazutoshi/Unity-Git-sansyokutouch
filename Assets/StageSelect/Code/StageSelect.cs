22cf876cace68298b51f11b3ffc85590b56675a6
JC-21\220410 <220410@jc-21.jp>
2024-01-11 11:25:58 +0900

    いろいろと追加しています

---
+++ Assets/StageSelect/Code/StageSelect.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class StageSelect : MonoBehaviour
+{
+    public int StageNumber;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch()
+        
+    }
+}
